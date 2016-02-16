02f0a13651ccb1d193f103a6cf67bee58a8973c4
Kai-Qian <kaiq@andrew.cmu.edu>
2016-02-16 15:42:53 -0500

    delete

--- webapps/socialnetwork/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from socialnetwork.models import *
-
-# Register your models here.
-admin.site.register(Posting)
-admin.site.register(Profile)
-admin.site.register(Followings)
